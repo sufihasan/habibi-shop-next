c0c1b4a1df8e2f2e3c0e20eaa7ca9807c500c5ce
sufihasan <hasan6nh@gmail.com>
2026-02-17 15:58:37 +0600

    make overview page inside dashboard/overview

---
+++ src/components/dashboard/AppSidebar.tsx
@@ -0,0 +1,141 @@
+import { GalleryVerticalEnd } from "lucide-react";
+import * as React from "react";
+
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarMenuSub,
+    SidebarMenuSubButton,
+    SidebarMenuSubItem,
+    SidebarRail,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
+
+const data = {
+    navMain: [
+        {
+            title: "Getting Started",
+            url: "#",
+            items: [
+                {
+                    title: "Home",
+                    url: "/",
+                },
+                {
+                    title: "Overview",
+                    url: "overview",
+                    isActive: true,
+                },
+            ],
+        },
+        {
+            title: "Event Management",
+            url: "#",
+            items: [
+                {
+                    title: "All Events",
+                    url: "all-events",
+                },
+                {
+                    title: "Create Event",
+                    url: "create-event",
+                },
+            ],
+        },
+        {
+            title: "API Reference",
+            url: "#",
+            items: [
+                {
+                    title: "Components",
+                    url: "#",
+                },
+                {
+                    title: "File Conventions",
+                    url: "#",
+                },
+            ],
+        },
+        {
+            title: "Architecture",
+            url: "#",
+            items: [
+                {
+                    title: "Accessibility",
+                    url: "#",
+                },
+                {
+                    title: "Turbopack",
+                    url: "#",
+                },
+            ],
+        },
+        {
+            title: "Community",
+            url: "#",
+            items: [
+                {
+                    title: "Contribution Guide",
+                    url: "#",
+                },
+            ],
+        },
+    ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+    return (
+        <Sidebar {...props}>
+            <SidebarHeader>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton size="lg" asChild>
+                            <a href="#">
+                                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                                    <GalleryVerticalEnd className="size-4" />
+                                </div>
+                                <button className="flex items-center gap-2 font-semibold text-xl hover:opacity-80 transition-opacity">
+                                    <Link href="/">
+                                        <span>Next Event</span>
+                                    </Link>
+                                </button>
+                            </a>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarMenu>
+                        {data.navMain.map((item) => (
+                            <SidebarMenuItem key={item.title}>
+                                <SidebarMenuButton asChild>
+                                    <a href={item.url} className="font-medium">
+                                        {item.title}
+                                    </a>
+                                </SidebarMenuButton>
+                                {item.items?.length ? (
+                                    <SidebarMenuSub>
+                                        {item.items.map((item) => (
+                                            <SidebarMenuSubItem key={item.title}>
+                                                <SidebarMenuSubButton asChild isActive={item.isActive}>
+                                                    <a href={item.url}>{item.title}</a>
+                                                </SidebarMenuSubButton>
+                                            </SidebarMenuSubItem>
+                                        ))}
+                                    </SidebarMenuSub>
+                                ) : null}
+                            </SidebarMenuItem>
+                        ))}
+                    </SidebarMenu>
+                </SidebarGroup>
+            </SidebarContent>
+            <SidebarRail />
+        </Sidebar>
+    );
+}
